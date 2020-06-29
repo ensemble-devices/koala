b069056b15c267fccd50b5a022f4ef5d346ac24d
Miles Chesney <miles@ensemble-devices.com>
2020-06-29 19:14:47 +0100

    remove secondary dependencies from install_requires

--- setup.py
+++ setup.py
@@ -67,9 +67,6 @@ if __name__ == '__main__':
             'openpyxl >= 3.0.3',
             'numpy >= 1.14.2',
             'numpy-financial>=1.0.0',
-            'Cython >= 0.29.15',
-            'lxml >= 4.5.0',
-            'scipy>=1.0.0',
-            'python-dateutil>=2.8.0'
+            'scipy>=1.0.0'
         ]
     )
